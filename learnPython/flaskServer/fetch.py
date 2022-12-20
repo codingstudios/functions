fd3aa9dbfd22255d0feae81da0573dfef8901105
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-21 00:02:40 +0800

    Update fetch.py

--- learnPython/flaskServer/fetch.py
+++ learnPython/flaskServer/fetch.py
@@ -1,6 +1,6 @@
 import requests, json
 
-response = requests.get("https://api.github.com/users/leecheeyong")
+response = requests.get("https://api.github.com/users/leecheeyong", headers={"Accept": "application/json"})
 user = response.json();
 
 print(user)
